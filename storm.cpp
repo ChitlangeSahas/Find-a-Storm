4e160ef12e25b5657585c01ae6de50aef605fa16
chitlangesahas <chitlangesahas@gmail.com>
2019-10-14 16:19:22 -0700

    Added more code

--- storm.cpp
+++ storm.cpp
@@ -104,7 +104,96 @@ private:
     HashNode *next;
 };
 
+class HashMap {
+public:
+    HashMap() {
+        // construct zero initialized hash table of size
+        table = new HashNode *[TABLE_SIZE]();
+    }
 
+    ~HashMap() {
+        // destroy all buckets one by one
+        for (int i = 0; i < TABLE_SIZE; ++i) {
+            HashNode<K, V> *entry = table[i];
+            while (entry != NULL) {
+                HashNode<K, V> *prev = entry;
+                entry = entry->getNext();
+                delete prev;
+            }
+            table[i] = NULL;
+        }
+        // destroy the hash table
+        delete [] table;
+    }
+
+    bool get(const K &key, V &value) {
+        unsigned long hashValue = hashFunc(key);
+        HashNode *entry = table[hashValue];
+
+        while (entry != NULL) {
+            if (entry->getKey() == key) {
+                value = entry->getValue();
+                return true;
+            }
+            entry = entry->getNext();
+        }
+        return false;
+    }
+
+    void put(int &key, string &value) {
+        unsigned long hashValue = hashFunc(key);
+        HashNode *prev = NULL;
+        HashNode *entry = table[hashValue];
+
+        while (entry != NULL && entry->getKey() != key) {
+            prev = entry;
+            entry = entry->getNext();
+        }
+
+        if (entry == NULL) {
+            entry = new HashNode(key, value);
+            if (prev == NULL) {
+                // insert as first bucket
+                table[hashValue] = entry;
+            } else {
+                prev->setNext(entry);
+            }
+        } else {
+            // just update the value
+            entry->setValue(value);
+        }
+    }
+
+    void remove(int &key) {
+        unsigned long hashValue = hashFunc(key);
+        HashNode *prev = NULL;
+        HashNode *entry = table[hashValue];
+
+        while (entry != NULL && entry->getKey() != key) {
+            prev = entry;
+            entry = entry->getNext();
+        }
+
+        if (entry == NULL) {
+            // key not found
+            return;
+        }
+        else {
+            if (prev == NULL) {
+                // remove first bucket of the list
+                table[hashValue] = entry->getNext();
+            } else {
+                prev->setNext(entry->getNext());
+            }
+            delete entry;
+        }
+    }
+
+private:
+    // hash table
+    HashNode **table;
+    F hashFunc;
+};
 
 /*
 * convert the string into vector tokens
