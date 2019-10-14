5fb579208a98e6239b09d6cbee53d1f39cae6ee5
chitlangesahas <chitlangesahas@gmail.com>
2019-10-14 10:57:35 -0700

    added code to handle requests

--- storm.cpp
+++ storm.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+#include <vector>
 
 using namespace std;
 
@@ -9,13 +10,14 @@ using namespace std;
 */
 class LinkedList
 {
-private:
+
+	private:
 	struct node
 	{
 		string data;
 		node *next;
 	};
-public:
+	public:
 	node *head , *tail;
 	int size;
 	LinkedList()
@@ -64,14 +66,34 @@ public:
 			cout << "list is empty\n";
 		}
 	}
-	// ~LinkedList();
+
+	// ~LinkedList();};
 };
 
 /*
+* convert the string into vector tokens
+*/
+std::vector<string> 	str_to_vector_tokens(std::string str, std::string delim)
+{
+    std::vector<std::string> tokens;
+    size_t prev = 0, pos = 0;
+    do
+    {
+        pos = str.find(delim, prev);
+        if (pos == std::string::npos) pos = str.length();
+        std::string token = str.substr(prev, pos-prev);
+        if (!token.empty()) tokens.push_back(token);
+        prev = pos + delim.length();
+    }
+    while (pos < str.length() && prev < str.length());
+    return tokens;
+}
+/*
 * Get the command line query as a string 
 */
-string	get_query(char const *argv[])
+std::vector<string>	parse_query(char const *argv[])
 {
+
 	int i = 1;
 	string query = "";
 	while(argv[i])
@@ -80,13 +102,59 @@ string	get_query(char const *argv[])
 		query += ",";
 		i++; 
 	}
-	return query;}
+	std::vector<string> query_v;
 
+	query_v = str_to_vector_tokens(query, ",");
+	return query_v;
+}
+
+/*
+* helper function to check if 
+* a string is a number
+*/
+bool is_number(const std::string& s)
+{
+    std::string::const_iterator it = s.begin();
+    while (it != s.end() && std::isdigit(*it)) ++it;
+    return !s.empty() && it == s.end();
+}
 
 int 	main(int argc, char const *argv[])
 {
-	string query = get_query(argv);
-	
-	cout << query;
+	std::vector<string> query = parse_query(argv);
+
+	// find
+	if (query[0] == "find")
+	{
+		// find event
+		if (query[1] == "event")
+		{
+			/* code */
+		}
+		// find max
+		else if (query[1] == "max")
+		{
+			// find max fatality
+			if (query[2] == "fatality")
+			{
+				/* code */
+			}
+			// find max 
+			else if (is_number(query[2]))
+			{
+				/* code */
+			}
+			else cout << "INVALID QUERY";
+		}
+
+		else cout << "INVALID QUERY";
+	}
+	else if(query[0] == "range")
+	{
+	  	/* code */
+	}
+
+	else cout << "INVALID QUERY";
+
 	return 0;
-}			
+}
