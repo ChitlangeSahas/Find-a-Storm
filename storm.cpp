cfbb7c1aaa260341f186d4972cf559ed032d1a22
chitlangesahas <chitlangesahas@gmail.com>
2019-10-21 11:01:55 -0700

    Everything for milestone works

--- storm.cpp
+++ storm.cpp
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string.h>
-// #include <vector>
+#include <math.h>
 #include "defn.h"
+#include <climits>
 
 /*
 * No vectors? I made an Arraylist class.
@@ -111,6 +113,9 @@ ArrayList<T>::~ArrayList() {
 }
 
 #define ROOT_FOLDER "./1950-1952/"
+
+
+ArrayList<int> years;
 
 using namespace std;
 
@@ -372,6 +377,14 @@ ArrayList<string>	parse_query(char const *argv[])
 	return query_v;
 }
 
+ArrayList<string>	parse_query_cin(string query)
+{
+	ArrayList<string> query_v;
+
+	query_v = str_to_vector_tokens(query, " ");
+	return query_v;
+}
+
 /*
 * helper function to check if 
 * a string is a number
@@ -386,30 +399,67 @@ bool is_number(string& s)
 /*
 * convert the file into a vector. 
 */
-ArrayList<string> 	file_to_vector()
+ArrayList<string> 	detail_file_to_array_list()
 {
-	int line_n = 0;
-	string FILE_PATH = string(ROOT_FOLDER) + "details-1950.csv";
-	string line;
-	ifstream file_stream;
-
-	// open file and put the file into a 2d vector.
-	char fp[FILE_PATH.size() + 1];
-	strcpy(fp, FILE_PATH.c_str());
-
-	file_stream.open(fp);
-	// get rid of the first line
-	getline(file_stream, line); 
-
 	ArrayList<string> lines;
-
-	// make a vector 
-	while(getline(file_stream, line).good())
+	for (int i = 0; i < years.size(); ++i)
 	{
-		lines.push_back(line);
+		int line_n = 0;
+		stringstream ss;
+		ss << years[i];
+		string year = ss.str();
+		string FILE_PATH = string(ROOT_FOLDER) + "details-"+ year + ".csv";
+		string line;
+		ifstream file_stream;
+
+		// open file and put the file into a 2d vector.
+		char fp[FILE_PATH.size() + 1];
+		strcpy(fp, FILE_PATH.c_str());
+
+		file_stream.open(fp);
+		// get rid of the first line
+		getline(file_stream, line); 
+
+		// make a vector 
+		while(getline(file_stream, line).good())
+		{
+			lines.push_back(line);
+		}
 	}
 	return lines;
 }
+
+ArrayList<string> 	fatalities_file_to_array_list()
+{
+	ArrayList<string> lines;
+
+	for (int i = 0; i < years.size(); ++i)
+	{
+		int line_n = 0;
+		stringstream ss;
+		ss << years[i];
+		string year = ss.str();
+		string FILE_PATH = string(ROOT_FOLDER) + "fatalities-"+ year +".csv";
+		string line;
+		ifstream file_stream;
+
+		// open file and put the file into a 2d vector.
+		char fp[FILE_PATH.size() + 1];
+		strcpy(fp, FILE_PATH.c_str());
+
+		file_stream.open(fp);
+		// get rid of the first line
+		getline(file_stream, line); 
+
+		// make a vector 
+		while(getline(file_stream, line).good())
+		{
+			lines.push_back(line);
+		}
+	}
+	return lines;
+}
+
 
 int 	convert_to_digits(string str)
 {
@@ -443,6 +493,7 @@ int 	convert_to_digits(string str)
 */
 void 	print_storm_event(storm_event se)
 {
+	cout << "=====" << endl;
 	cout << "EVENT ID : " << se.event_id << "\n";
 	cout << "STATE : " << se.state << "\n";	
 	cout << "YEAR : " << se.year << "\n";	
@@ -457,28 +508,83 @@ void 	print_storm_event(storm_event se)
 	cout << "DAMAGE PROPERTY : " << se.damage_property << "\n";
 	cout << "DAMAGE CROPS : " << se.damage_crops << "\n";
 	cout << "TOR F Scale : " << se.tor_f_scale << "\n";	 
+
+	if (se.f->event_id != 0)
+	{
+		cout << "Fatality Information: " << endl;
+		cout << "FATALITY ID : " << se.f->fatality_id << "\n";
+		cout << "EVENT ID : " << se.f->event_id << "\n";
+		cout << "FATALITY TYPE : " << se.f->fatality_type << "\n";
+		cout << "FATALITY DATE : " << se.f->fatality_date << "\n";
+		cout << "FATALITY AGE : " << "Information N/A"<< "\n";
+		cout << "FATALITY SEX : " << "Information N/A" << "\n";
+		cout << "FATALITY LOC : " << "Information N/A" << "\n";
+	}
+	else
+	{
+		cout << "No Fatalities.\n";
+	}
+	cout << "=====\n";
+}
+
+/*
+* Is it a prime number?
+*/
+bool TestForPrime( int val )
+{
+    int limit, factor = 2;
+
+    limit = (long)( sqrtf( (float) val ) + 0.5f );
+    while( (factor <= limit) && (val % factor) )
+        factor++;
+
+    return( factor > limit );
 }
 
 int 	main(int argc, char const *argv[])
 {
-	ArrayList<string> details_lines = file_to_vector();
+	ArrayList<string> q = parse_query(argv);
+
+	int n_years = stoi(q[0]);
+
+	for (int i = 0; i < n_years; ++i)
+	{
+		years.push_back(stoi(q[i + 1]));
+	}
+
+	ArrayList<string> details_lines = detail_file_to_array_list();
+	ArrayList<string> fatality_lines = fatalities_file_to_array_list();
 
 	int index = 0;
 	int TABLE_SIZE = details_lines.size();
 
-	HashTable table(TABLE_SIZE);
-	storm_event *event = (storm_event*) malloc(TABLE_SIZE * sizeof(storm_event));
+	int next_prime_number_table_size = 2 * TABLE_SIZE;
 
+	bool found = false;
+	while(!found)
+	{
+		next_prime_number_table_size += 1;
+		if (TestForPrime(next_prime_number_table_size))
+		{
+			found = true;
+			break;
+		}		
+	}
+
+	HashTable table(next_prime_number_table_size);
+	storm_event *event = (storm_event*) malloc(next_prime_number_table_size * sizeof(storm_event));
 
 	for (int i = 0; i < TABLE_SIZE; ++i)
 	{
 		ArrayList<string> detail_line = str_to_vector_tokens(details_lines[i], ",");
+
+		// make the hash node entry
 		hash_table_entry e;
 			e.event_id = stoi(detail_line[0]);
 			e.event_index = index;
 			e.year = stoi(detail_line[2]);
 
-
+		// add the storm event to the entry
 		storm_event se;
 			se.event_id = stoi(detail_line[0]);
 			strcpy(se.state, detail_line[1].c_str());
@@ -495,6 +601,28 @@ int 	main(int argc, char const *argv[])
 			se.damage_crops = stoi(detail_line[12]);
 			strcpy(se.tor_f_scale, detail_line[13].c_str());
 
+			// add the fatality event to the entry
+			fatality_event *fe = new fatality_event;
+			for (int j = 0; j < fatality_lines.size(); ++j)
+			{
+				ArrayList<string> fatality_line = str_to_vector_tokens(fatality_lines[j] , ",");
+				int f_event_id = stoi(fatality_line[1]);
+
+				if (f_event_id == se.event_id)
+				{
+					fe->fatality_id = stoi(fatality_line[0]);
+					fe->event_id = stoi(fatality_line[1]);
+					fe->fatality_type = fatality_line[2][0];
+					strcpy(fe->fatality_date, fatality_line[3].c_str());
+
+					fe->fatality_age = 0;
+					fe->fatality_sex = 'Z';
+					// strcpy(fe.fatality_location, fatality_line[6].c_str());
+
+				}
+			}
+			(se.f) = fe; // link the fatality_event to the storm event 
+
 			// add the storm event to the damn storm event array
 			storm_event *ptr = event;
 			for (int i = 0; i < index; ++i)
@@ -503,74 +631,103 @@ int 	main(int argc, char const *argv[])
 			}
 			*ptr = se; 
 
-		index++;
+		index++; // advance the index
 		table.add_to_table(e);
 	}
 
-	ArrayList<string> query = parse_query(argv);
+	
+	int n_queries = 0;
 
-	// find
-	if (query[0] == "find")
+	cin >> n_queries;
+	cin.get();
+
+	ArrayList<string> queries;
+	string query_ = "";
+	
+	for (int i = 0; i < n_queries; ++i)
 	{
-		// find event
-		if (query[1] == "event")
+		query_ = "";
+		getline(cin, query_);
+		queries.push_back(query_);
+		// cin.get();
+	}
+	
+
+	for (int i = 0; i < n_queries; ++i)
+	{
+		ArrayList<string> query = parse_query_cin(queries[i]);
+	
+		// find
+		if (query[0] == "find")
 		{
-
-			int ei = table.find(stoi(query[2])).event_index;
-			storm_event *ptr = event;
-			for (int i = 0; i < ei; ++i)
+			// find event
+			if (query[1] == "event")
 			{
-				ptr++;
+				hash_table_entry entry_found = table.find(stoi(query[2]));
+				int ei = entry_found.event_index;
+
+				storm_event *ptr = event;
+				for (int j = 0; j < ei; ++j)
+				{
+					ptr++;
+				}
+				if (entry_found.event_id != -1)
+				{
+					print_storm_event(*ptr);
+				}
+				else
+				{
+					cout << "Storm Event not found." << endl;
+				}
+
 			}
-			print_storm_event(*ptr);
+			// find max
+			else if (query[1] == "max")
+			{
+				// find max fatality
+				if (query[2] == "fatality")
+				{
+					/* code */
+				}
+				// find max 
+				else if (is_number(query[2]))
+				{
+					/* code */
+				}
+				else cout << "INVALID QUERY" << endl;
+			}
+			else cout << "INVALID QUERY" << endl;
 		}
-		// find max
-		else if (query[1] == "max")
+		// range
+		else if(query[0] == "range")
 		{
-			// find max fatality
-			if (query[2] == "fatality")
-			{
-				/* code */
-			}
-			// find max 
-			else if (is_number(query[2]))
-			{
-				/* code */
-			}
-			else cout << "INVALID QUERY";
+			// range all
+		  	if (query[1] == "all")
+		  	{
+		  		/* code */
+		  	}
+		  	// range <yyyy> 
+		  	else if (is_number(query[1]))
+		  	{
+		  		/* code */
+		  	}
+		  	else cout << "INVALID QUERY" << endl;
 		}
-		else cout << "INVALID QUERY";
-	}
-	// range
-	else if(query[0] == "range")
-	{
-		// range all
-	  	if (query[1] == "all")
-	  	{
-	  		/* code */
-	  	}
-	  	// range <yyyy> 
-	  	else if (is_number(query[1]))
-	  	{
-	  		/* code */
-	  	}
-	  	else cout << "INVALID QUERY";
-	}
-	// storm 1 1950
+		// storm 1 1950
 
-	else if(is_number(query[0]))
-	{
-		int n_years = stoi(query[0]);
-		ArrayList<int> years;
-		// make a vector of years one wants to get data of.
-		for (int i = 1; i <= n_years; ++i)
-			{
-				years.push_back(stoi(query[i]));
-			}	
-		
+		else if(is_number(query[0]))
+		{
+			int n_years = stoi(query[0]);
+			ArrayList<int> years;
+			// make a vector of years one wants to get data of.
+			for (int k = 1; k <= n_years; ++k)
+				{
+					years.push_back(stoi(query[k]));
+				}	
+			
+		}
+		else cout << "INVALID QUERY" << endl;
 	}
-	else cout << "INVALID QUERY";
-
 
 	return 0;
 }
---
+++ test.cpp
@@ -0,0 +1,11 @@
+struct site
+{
+    int pages = 200;
+    char name[] = "Quiz";
+};
+
+int main(int argc, char const *argv[])
+{
+	/* code */
+	return 0;
+}
